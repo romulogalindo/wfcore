c89b5692239774c686cf0d8c8731768e412639a7
rgalindo <romulogalindo@gmail.com>
2018-12-27 19:18:33 -0500

    WF-CORE update 2018-12-17 up001

--- src/main/java/com/acceso/wfcore/kernel/Application.java
+++ src/main/java/com/acceso/wfcore/kernel/Application.java
@@ -4,13 +4,14 @@ import com.acceso.wfcore.daos.SystemDAO;
 import com.acceso.wfcore.dtos.SystemTreeDTO;
 import com.acceso.wfcore.services.CacheService;
 import com.acceso.wfcore.services.DataSourceService;
+import com.acceso.wfcore.services.JavaScriptService;
 import com.acceso.wfcore.utils.Values;
 import com.acceso.wfweb.controls.LoginCTRL;
 import com.acceso.wfweb.web.Root;
 
 public class Application {
     public static final String APPLICATION_PROJECT_NAME = "ZERO DAWN";
-    public static final String APPLICATION_NAME = "Workfloa AIO2";
+    public static final String APPLICATION_NAME = "Workflow AIO2";
     public static final String APPLICATION_DEFAULT_LOCALE = "ES_PE";
 
     private LoginCTRL loginCTRL;
@@ -18,17 +19,20 @@ public class Application {
     //se creara el servicio aqui momentaneamente
     public CacheService cacheService;
     public DataSourceService dataSourceService;
+    public JavaScriptService javaScriptService;
 
 
     public Application() {
         loginCTRL = ApplicationManager.getLoginCTRL();
         cacheService = new CacheService("AIOCACHE");
         dataSourceService = new DataSourceService("DataSourceService");
+        javaScriptService = new JavaScriptService("JSEngine");
     }
 
     public void run() {
         cacheService.start();
         dataSourceService.start();
+        javaScriptService.start();
 
         //creamos la cache del menu
         cacheService.getZeroDawnCache().createSpace(Values.CACHE_MAIN_MENUTREE, String.class, Object.class);
@@ -45,7 +49,7 @@ public class Application {
         Root mainTree = ApplicationManager.buildRootTree(systemTreeDTO);
 
         //poner rootTree en la cache
-        cacheService.getZeroDawnCache().getSpace(Values.CACHE_MAIN_MENUTREE).put("ROOT_TREE",mainTree);
+        cacheService.getZeroDawnCache().getSpace(Values.CACHE_MAIN_MENUTREE).put("ROOT_TREE", mainTree);
 
         System.out.println("mainTree = " + mainTree);
 
@@ -74,5 +78,11 @@ public class Application {
 
     public void setDataSourceService(DataSourceService dataSourceService) {
         this.dataSourceService = dataSourceService;
+    }
+
+    public void destroy(){
+        cacheService.stop();
+        dataSourceService.stop();
+        javaScriptService.stop();
     }
 }
---
+++ src/main/java/com/acceso/wfcore/services/JavaScriptService.java
@@ -0,0 +1,79 @@
+package com.acceso.wfcore.services;
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptEngineManager;
+import java.util.List;
+
+public class JavaScriptService extends Service {
+    ScriptEngineManager manager;
+    ScriptEngine engine_nativejs;
+    ScriptEngine engine_nashornjs;
+    ScriptEngine engine_graalvmjs;
+
+    public JavaScriptService(String serviceName) {
+        super(serviceName);
+
+        //Levantar el motor de JS
+        manager = new ScriptEngineManager();
+        engine_nativejs = manager.getEngineByName("javascript");
+        engine_nashornjs = manager.getEngineByName("nashorn");
+        engine_graalvmjs = manager.getEngineByName("Graal.js");
+        List<ScriptEngineFactory> scriptEngineFactories = manager.getEngineFactories();
+        for (ScriptEngineFactory scriptEngineFactory : scriptEngineFactories) {
+            System.out.println("scriptEngineFactory = " + scriptEngineFactory);
+        }
+    }
+
+    @Override
+    public void start() {
+        String script_Base = "(function() { return this; })";
+
+        /*ENGINE 1*/
+        try {
+            Object fn = engine_nativejs.eval(script_Base);
+            Invocable inv = (Invocable) engine_nativejs;
+            Object result = inv.invokeMethod(fn, "call", fn);
+            System.out.println("(ENGINE1)result = " + result);
+        } catch (Exception ep) {
+            System.out.println("ENGINE 1 (error)");
+            ep.printStackTrace();
+        }
+
+        /*ENGINE 2*/
+        try {
+            Object fn = engine_nashornjs.eval(script_Base);
+            Invocable inv = (Invocable) engine_nashornjs;
+            Object result = inv.invokeMethod(fn, "call", fn);
+            System.out.println("(ENGINE2)result = " + result);
+        } catch (Exception ep) {
+            System.out.println("ENGINE 2 (error)");
+            ep.printStackTrace();
+        }
+
+        /*ENGINE 3*/
+//        org.graalvm.polyglot.Engine.
+        Context context = Context.create();
+        Value array = context.eval("js", "[1,2,42,4]");
+        int result2 = array.getArrayElement(2).asInt();
+
+        try {
+            Object fn = engine_graalvmjs.eval(script_Base);
+            Invocable inv = (Invocable) engine_graalvmjs;
+            Object result = inv.invokeMethod(fn, "call", fn);
+            System.out.println("(ENGINE3)result = " + result);
+        } catch (Exception ep) {
+            System.out.println("ENGINE 3 (error)");
+            ep.printStackTrace();
+        }
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
